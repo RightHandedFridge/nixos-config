393f7f5c8ad322fedff0996eb4303ce9d739186f
RHFridge <rhfridge@protonmail.com>
2025-04-29 20:50:59 +0100

    Fix: Use Let, In for self option reference

--- modules/nixos/features/hm.nix
+++ modules/nixos/features/hm.nix
@@ -8,13 +8,15 @@
 
   # Define a mapping from hostnames to their respective paths
   homeNixPath = host: builtins.toString ../../../hosts/${host}/home.nix;
+
+  cfg = config.modules.system.hm;
 in {
   options.modules.system.hm = {
     enable = lib.mkEnableOption "Enable Home Manager module";
   };
 
   # Apply configuration based on the options
-  config = lib.mkIf config.modules.hm.enable {
+  config = lib.mkIf cfg.enable {
     programs.fuse.userAllowOther = true; #Required for impermenance
     home-manager = {
       extraSpecialArgs = {inherit inputs;};
