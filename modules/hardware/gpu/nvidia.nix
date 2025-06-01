f4be3c266c12505e1cb664f1a3eb0c4b7b20eb07
RHFridge <rhfridge@protonmail.com>
2025-06-01 14:26:35 +0100

    Fix: Always declare Open

--- modules/hardware/gpu/nvidia.nix
+++ modules/hardware/gpu/nvidia.nix
@@ -28,7 +28,7 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = lib.mkIf (config.vars.host != "homeServer") true;
+    open = if config.vars.host != "homeServer" then true else false;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
