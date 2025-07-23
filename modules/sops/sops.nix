05b254abf5620ec4211a2b4d314cc422b0c2464a
RHFridge <28656853+RightHandedFridge@users.noreply.github.com>
2025-07-23 20:11:56 +0100

    MR KRABS! I WANT TO GO TO BED!

--- modules/sops/sops.nix
+++ modules/sops/sops.nix
@@ -15,7 +15,10 @@
   sops = {
     defaultSopsFile = ./secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
-    age.keyFile = "/persist/home/${config.vars.user}/.config/sops/age/keys.txt";
+    age.keyFile =
+      if config.modules.system.impermanence.enable
+      then "/persist/home/${config.vars.user}/.config/sops/age/keys.txt"
+      else "/home/${config.vars.user}/.config/sops/age/keys.txt";
 
     secrets = {
       x280pass.neededForUsers = true;
