c5368e9d6bd7d27bfbaeac5fd482ce9f77e89a83
RHFridge <rhfridge@local.me>
2025-10-19 14:26:52 +0100

    me when I wrong syntax

--- modules/de/hyprland/features/hyprland.nix
+++ modules/de/hyprland/features/hyprland.nix
@@ -60,7 +60,7 @@ in {
           "$modapp, S, exec, $menu"
           "$modapp, W, exec, $browser"
           "$modapp, E, exec, $files"
-          "$modapp SHIFT, S, alacritty -e pulsemixer"
+          "$modapp SHIFT, S, exec, alacritty -e pulsemixer"
 
           # Focus Shift
           "$mod, J, movefocus, l"
