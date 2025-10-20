796cc72d7cff3e0d6e1f7fd62a73d76989cebff0
RHFridge <28656853+RightHandedFridge@users.noreply.github.com>
2025-10-20 12:48:50 +0100

    Fix: osConfig -> config

--- modules/de/hyprland/features/ly.nix
+++ modules/de/hyprland/features/ly.nix
@@ -1,5 +1,5 @@
-{ lib, osConfig, ... }:{
-  services.displayManager.ly = lib.mkIf osConfig.modules.de.hyprland.enable {
+{ lib, config, ... }:{
+  services.displayManager.ly = lib.mkIf config.modules.de.hyprland.enable {
     enable = true;
     x11Support = false;
   };
