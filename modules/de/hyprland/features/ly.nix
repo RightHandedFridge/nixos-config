5e8d0831a01366016e697824a4d5bf7c8a0a4ccd
RHFridge <28656853+RightHandedFridge@users.noreply.github.com>
2025-10-20 14:07:07 +0100

    Fix: Attempt to Fix ly launching Hyprland with no config

--- modules/de/hyprland/features/ly.nix
+++ modules/de/hyprland/features/ly.nix
@@ -1,5 +1,23 @@
-{ lib, config, ... }:{
+{ lib, config, pkgs, ... }:
+{
   services.displayManager.ly = lib.mkIf config.modules.de.hyprland.enable {
     enable = true;
   };
+
+  services.displayManager.sessionPackages = [
+    (
+      (pkgs.writeTextDir "share/wayland-sessions/hyprland.desktop" ''
+        [Desktop Entry]
+        Name=Hyprland
+        Comment=Hyprland Wayland Compositor
+        Exec=${pkgs.greetd.hyprland}/bin/Hyprland
+        Type=Application
+      '')
+      .overrideAttrs
+      (_: {
+        passthru.providedSessions = ["hyprland"];
+      })
+    )
+  ];
+  
 }
