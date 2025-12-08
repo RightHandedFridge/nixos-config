ae25d17971b211831c530eea46c5e551b622002a
RHFridge <28656853+RightHandedFridge@users.noreply.github.com>
2025-12-08 18:10:24 +0000

    Change: Hyprsunset -> Sunsetr

--- modules/de/hyprland/bundles/hyprland-hm.nix
+++ modules/de/hyprland/bundles/hyprland-hm.nix
@@ -1,11 +1,11 @@
-{ ... }:{
+{...}: {
   imports = [
     ../features/hyprland.nix
     ../features/swaync.nix
     ../features/swayosd.nix
     ../features/waybar.nix
     ../features/hyprpaper.nix
-    ../features/hyprsunset.nix
+    ../features/sunsetr.nix
     ../../stylix/stylix-hm.nix
     ../features/rofi.nix
     ../features/hyprlock.nix
---
+++ modules/de/hyprland/features/sunsetr.nix
@@ -0,0 +1,59 @@
+{
+  lib,
+  osConfig,
+  pkgs,
+  ...
+}: {
+  config = lib.mkIf osConfig.modules.de.hyprland.enable {
+    home.packages = with pkgs; [
+      sunsetr
+    ];
+
+    xdg.configFile."sunsetr/sunsetr.toml".text = ''
+      #[Backend]
+      backend = "hyprland"
+      transition_mode = "finish_by"
+
+      #[Smoothing]
+      smoothing = true
+      startup_duration = 0.5
+      shutdown_duration = 0.5
+      adaptive_interval = 1
+
+      #[Time-based config]
+      night_temp = 3000
+      day_temp = 6500
+      night_gamma = 100
+      day_gamma = 100
+      update_interval = 60
+
+      #[Static config]
+      static_temp = 6500
+      static_gamma = 100
+
+      #[Manual transitions]
+      sunset = "19:00:00"
+      sunrise = "05:00:00"
+      transition_duration = 30
+    '';
+
+    systemd.user.services.sunsetr = {
+      Unit = {
+        Description = "Sunsetr blue-light filtering";
+        After = ["graphical-session.target"];
+        PartOf = ["graphical-session.target"];
+      };
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.sunsetr}/bin/sunsetr -b";
+        Restart = "on-failure";
+        RestartSec = 1;
+      };
+
+      Install = {
+        WantedBy = ["graphical-session.target"];
+      };
+    };
+  };
+}
