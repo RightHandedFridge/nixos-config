c5c7d54bb9c9da52212c52c66f19e2d14587fc0c
RHFridge <28656853+RightHandedFridge@users.noreply.github.com>
2026-02-15 15:51:03 +0000

    Fix: Capitalise "GUI"

--- modules/programs/keepassxc/hm.nix
+++ modules/programs/keepassxc/hm.nix
@@ -1,9 +1,13 @@
-{ config, lib, ... }: {
+{
+  config,
+  lib,
+  ...
+}: {
   home-manager.users.${config.vars.user} = lib.mkIf config.modules.programs.keepassxc.enable {
     programs.keepassxc = {
       enable = true;
       settings = {
-        gui = {
+        GUI = {
           ApplicationTheme = "dark";
           HidePasswords = true;
         };
