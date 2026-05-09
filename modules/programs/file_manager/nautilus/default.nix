a8ef6d68d90cab966e83ae0993b077fbfbb223eb
RHFridge <28656853+RightHandedFridge@users.noreply.github.com>
2026-05-09 03:07:39 +0100

    Default File Manager

--- modules/programs/file_manager/nautilus/default.nix
+++ modules/programs/file_manager/nautilus/default.nix
@@ -5,5 +5,6 @@
 
   imports = [
     ./nixos.nix
+    ./hm.nix
   ];
 }
---
+++ modules/programs/file_manager/nautilus/hm.nix
@@ -0,0 +1,16 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  home-manager.users.${config.vars.user} = lib.mkIf config.modules.programs.filemanager.nautilus.enable {
+    xdg.mimeApps = {
+      enable = true;
+
+      defaultApplications = {
+        "inode/directory" = ["nautilus.desktop"];
+      };
+    };
+  };
+}
