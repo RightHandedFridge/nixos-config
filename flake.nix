829a4c462ce1b1852114b7a5be78658ad7203d7b
RHFridge <28656853+RightHandedFridge@users.noreply.github.com>
2026-06-15 13:53:58 +0100

    Flake Update

--- flake.nix
+++ flake.nix
@@ -42,10 +42,8 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-      #https://docs.noctalia.dev/getting-started/nixos/#nix-flakes
-      #Noctalia requires unstable since it depends on the latest quickshell
+      url = "github:noctalia-dev/noctalia/legacy-v4";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
     };
 
     # Secrets and filesystem
