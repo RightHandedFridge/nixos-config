5874d7bc89c301292e334f3499c9e612a1c1a8e5
RHFridge <28656853+RightHandedFridge@users.noreply.github.com>
2026-09-09 16:29:26 +0100

    Theme fix

--- modules/desktop/framework/stylix/qt.nix
+++ modules/desktop/framework/stylix/qt.nix
@@ -3,31 +3,30 @@
   pkgs,
   config,
   ...
-}:
-{
+}: {
   home-manager.users.${config.vars.user} = {
     home = {
       packages = with pkgs; [
-        qt6Packages.qt6ct 
+        qt6Packages.qt6ct
         libsForQt5.qt5ct
       ];
-      
+
       sessionVariables = {
         QT_QPA_PLATFORM = "wayland;xcb";
-        QT_STYLE_OVERRIDE = "kvantum"; 
+        QT_STYLE_OVERRIDE = "kvantum";
       };
     };
 
     qt = {
       enable = true;
-      platformTheme.name = "gtk3";
+      platformTheme.name = "qt6ct";
       style.name = "kvantum";
     };
 
     xdg.configFile = {
       # Link the parent folder 'WhiteSur' which houses both variants
       "Kvantum/WhiteSur".source = "${pkgs.whitesur-kde}/share/Kvantum/WhiteSur";
-      
+
       # Tell Kvantum to target the 'WhiteSurDark' configuration files inside that folder
       "Kvantum/kvantum.kvconfig".text = ''
         [General]
